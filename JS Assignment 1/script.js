33763cecf9c130f78531f85b6c2a3ed5d4f5eb39
Rahul Rathore <157625145+Rahul-Rathore1912@users.noreply.github.com>
2024-02-29 11:11:49 +0500

    Add files via upload

---
+++ JS Assignment 1/script.js
@@ -0,0 +1,17 @@
+document.write("<h1>Curriculam Vitae</h1>");
+document.write("<h2>Name : Rahul Rathore</h2>")
+document.write("<h2>Father's Name : Kewal Rathore</h2>")
+document.write("<h2>DOB : 19/12/2003</h2>")
+document.write("<h2>Contact : 03368314168</h2>")
+document.write("<h2>Maritual Status : Shadeed Single</h2>")
+document.write("<h2>CNIC : Unmarked</h2>")
+document.write("<h2>Addres : Sachal Goth Gulshan e iqbal Karachi </h2>")
+document.write("<h2>Work Experience as (Home Tutor)</h2>")
+document.write("<h2>Education: Intermediate</h2>")
+document.write("<h2>Hoobies: <br> (1)Reading <br> (2)Coding</h2>")
+document.write("<h2>Languages: (1)English (2)Urdu (3)Sindhi</h2>")
+
+
+
+
+
